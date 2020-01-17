5e349d36f6d6ce2d7d983ca40e9b8222db62129f
shubham <shbgtd@gmail.com>
2020-01-17 22:48:31 +0530

    eurekaserver

---
+++ BankLoanProject-EurekaServer/src/test/java/com/csrt/app/sme/main/BankLoanProjectEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csrt.app.sme.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankLoanProjectEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
