bc2447ea8b57721c34163e9f9dbba82597512ec3
samark895 <59816557+samark895@users.noreply.github.com>
2020-01-20 10:25:49 +0530

    Add files via upload

---
+++ LoanDisbursment_PaymentLedgerCreation/src/main/java/com/crts/app/sme/main/repository/LedgerUpdationDetailsRepository.java
@@ -0,0 +1,8 @@
+package com.crts.app.sme.main.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class LedgerUpdationDetailsRepository {
+
+}
---
+++ LoanDisbursment_PaymentLedgerCreation/src/main/java/com/crts/app/sme/main/service/LedgerUpdationDetailsImpl.java
@@ -0,0 +1,8 @@
+package com.crts.app.sme.main.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LedgerUpdationDetailsImpl implements LedgerUpdationDetailsI {
+
+}
